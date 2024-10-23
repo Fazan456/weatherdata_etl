adfde458f558cca91138877196fbe31f4683ed92
Mohamed Fazan2 <mohamedfazan76@gmail.com>
2024-10-24 00:08:05 +0530

    fixing code issues

--- dags/etl_weather.py
+++ dags/etl_weather.py
@@ -1,8 +1,10 @@
 from airflow import DAG
-from airflow.providers.https.hooks.http import HttpHook
-from airflow.providers.postgres.hooks.postgres import postgresHook
+from airflow.providers.http.hooks.http import HttpHook
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.decorators import task
 from airflow.utils.dates import days_ago
+import requests
+import json
 
 
 LATITUDE = '51.5074'
@@ -49,4 +51,39 @@ catchup = False) as dags:
         }
         return transformed_data
     
+    @task()
+    def load_weather_data(transformed_data):
+        pg_hook = postgresHook(postgres_conn_id = POSTGRES_CONN_ID)
+        conn = pg_hook.get_conn()
+        cursor = conn.cursor()
+
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS weather_data(
+                       latitude FLOAT,
+                       longitude FLOAT,
+                       temperature FLOAT,
+                       windspeed FLOAT,
+                       winddirection FLOAT,
+                       weathercode INT,
+                       timestamp TIMESTAMP DEFAULT_CURRENT_TIMESTAMP);""")
+        
+
+        cursor.execute("""
+        INSERT INTO weather_data(latitude, longitude, temperature, windspeed, winddirection,weathercode)
+        VALUES (%s, %s, %s, %s, %s,%s)
+                       """,(
+                           transformed_data["latitude"],
+                           transformed_data["longitude"],
+                           transformed_data["temperature"],
+                           transformed_data["windspeed"],
+                           transformed_data["winddirection"],
+                           transformed_data["weathercode"]
+                       ))
+        conn.commit()
+        cursor.close()
+
+    weatherdata = extract_weather_data()
+    transformed_data = transform_weather_data(weatherdata)
+    load_weather_data(transformed_data)
+
     
